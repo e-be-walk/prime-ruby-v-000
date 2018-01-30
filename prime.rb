945414e64c8653a57d8b9a0bc3ea5c50edeaa455
Erin Walker <erinb0317@hotmail.com>
2018-01-30 00:52:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 # Add  code here!
-def prime? n 
+def prime? n
   foundDivider = false
   for d in 2..(n-1)
     if (n % d) == 0
       return false
     end
-  elsif 
+  elsif
     foundDivider = ((n % d) == 0) || foundDivider
   end
   not foundDivider
